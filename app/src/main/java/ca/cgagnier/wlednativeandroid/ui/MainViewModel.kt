3cbe4953743e23087a77a023dc92a6695ac6dcd7
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2026-01-04 22:43:19 -0500

    Refactor: Simplify update check in MainViewModel
    - Use `.first()` to get the `lastUpdateCheckDate` instead of collecting the full flow. This simplifies the logic and avoids a long-lived collector.
    - Replaced the magic number for 24 hours in milliseconds with `DAYS.toMillis(1)` for better readability.

--- app/src/main/java/ca/cgagnier/wlednativeandroid/ui/MainViewModel.kt
+++ app/src/main/java/ca/cgagnier/wlednativeandroid/ui/MainViewModel.kt
@@ -8,7 +8,9 @@ import ca.cgagnier.wlednativeandroid.service.api.github.GithubApi
 import ca.cgagnier.wlednativeandroid.service.update.ReleaseService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit.DAYS
 import javax.inject.Inject
 
 private const val TAG = "MainViewModel"
@@ -22,16 +24,15 @@ class MainViewModel @Inject constructor(
 
     fun downloadUpdateMetadata() {
         viewModelScope.launch(Dispatchers.IO) {
-            userPreferencesRepository.lastUpdateCheckDate.collect {
-                val now = System.currentTimeMillis()
-                if (now < it) {
-                    Log.i(TAG, "Not updating version list since it was done recently.")
-                    return@collect
-                }
-                releaseService.refreshVersions(githubApi)
-                // Set the next date to check in minimum 24 hours from now.
-                userPreferencesRepository.updateLastUpdateCheckDate(now + (24 * 60 * 60 * 1000))
+            val lastCheckDate = userPreferencesRepository.lastUpdateCheckDate.first()
+            val now = System.currentTimeMillis()
+            if (now < lastCheckDate) {
+                Log.i(TAG, "Not updating version list since it was done recently.")
+                return@launch
             }
+            releaseService.refreshVersions(githubApi)
+            // Set the next date to check in minimum 24 hours from now.
+            userPreferencesRepository.updateLastUpdateCheckDate(now + DAYS.toMillis(1))
         }
     }
 }
